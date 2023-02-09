4e577bc61add5a829698a2431b584937dcedde57
magicaldave <smokydave@icloud.com>
2023-02-09 10:58:55 -0600

    Edit: Changes to imports as recommended by 2to3 tool.

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -3,13 +3,14 @@ import sys
 import re
 import string
 import json
-import urllib2
-import cookielib
+import urllib.request, urllib.error, urllib.parse
+from http.cookiejar import CookieJar
 import mechanize
 from bs4 import BeautifulSoup
-from base_parse import BaseParse
-from low_scraper import LowScraper
-from high_scraper import HighScraper
-from sys_scraper import SysScraper
-from test_file_scraper import TestFileScraper
-from read_scraper import ReadScraper
+from .base_parse import BaseParse
+from .low_scraper import LowScraper
+from .high_scraper import HighScraper
+from .sys_scraper import SysScraper
+from .test_file_scraper import TestFileScraper
+from .read_scraper import ReadScraper
+
